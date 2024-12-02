5c374210481c6f024230e73d0797b1d41d974eb8
Cedric12850 <cedric.deswarte81@gmail.com>
2024-12-02 09:42:27 +0100

    ajout table position et FK_player_Position dans table player

---
+++ .history/players/admin_20241202092026.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+
+from players.models import PLayers
+
+
+
+class PlayersAdmin(admin.ModelAdmin):
+    # Afficher des colonnes supplémentaires dans la liste des objets
+    list_display = ('name', 'firstName','thumbnail', 'age','team', 'poste', 'actif', 'injury' )
+
+    # Ajout d'un champ de recherche
+    search_fields = ('name', 'firstName', 'team', 'poste')
+
+    # Ajout de filtres dans la barre latérale
+    list_filter = ('team', 'poste', 'actif', 'injury')
+
+    # Pagination (afficher 10 éléments par page)
+    list_per_page = 10  
+
+    # Prévisualisation des images (si applicable)
+    readonly_fields = ('thumbnail_preview',)
+
+    def founded_year(self, obj):
+        return obj.founded.year
+    
+
+    def thumbnail_preview(self, obj):
+        if obj.thumbnail:
+            return f'<img src="{obj.thumbnail.url}" style="width: 50px; height: 50px;" />'
+        return "Pas d'image"
+    thumbnail_preview.allow_tags = True
+    thumbnail_preview.short_description = "Aperçu du logo"
+
+
+class PalyerPositionAdmin(admin.ModelAdmin):
+    # Afficher des colonnes supplémentaires dans la liste des objets
+    list_display = ("name", "abrev")
+
+    # Ajout d'un champ de recherche
+    search_fields = ("name" "abrev")
+
+    # Ajout de filtres dans la barre latérale
+    list_filter = ('name', 'abrev', 'actif', 'injury')
+
+
+# Enregistrement du modèle et de l'admin personnalisé
+admin.site.register(PLayers, PlayersAdmin)
---
+++ .history/players/models_20241202091630.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from team.models import Teams
+
+# Create your models here.
+class Player_position(models.Model):
+    name = models.CharField(max_length=60)
+    abrev = models.CharField(max_length=10)
+
+    
+class PLayers(models.Model):
+    name = models.CharField(max_length=60)
+    firstName = models.CharField(max_length=60)
+    poste = models.for(max_length=50, null=True, blank=True)
+    thumbnail = models.ImageField(upload_to="photo", null=True, blank=True)
+    age = models.DateField(default="1980-01-01")
+    actif = models.BooleanField()
+    injury = models.BooleanField()
+    team = models.ForeignKey(
+        Teams,
+        on_delete=models.SET_NULL,   # Définit la valeur sur NULL si l'équipe est supprimée
+        null=True,                   # Permet d'avoir des joueurs sans équipe
+        blank=True,                  # Rend le champ facultatif dans les formulaires
+        related_name="players"       # Facilite l'accès aux joueurs depuis une équipe
+    )
+    def __str__(self):           # Formate l'affichage en BdD
+        return f"{self.name} {self.firstName}"
+    
+    class Meta:
+        db_table = 'team_players'  # Utilise l'ancienne table existante
